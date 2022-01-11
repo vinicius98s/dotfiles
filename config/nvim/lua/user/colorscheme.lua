db12b94249910280c3d4ed1edda8e2d889f7d45b
Vinicius Sales <vinicius.2010.s@gmail.com>
2022-01-11 14:06:32 -0300

    fix colorscheme with tmux

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,3 +1,12 @@
+-- Tmux colorscheme
+vim.cmd [[
+  if exists('+termguicolors')
+    let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"
+    let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
+    set termguicolors
+  endif
+]]
+
 -- Ayu
 -- local colorscheme = "ayu"
 -- vim.g.ayucolor = "dark"
