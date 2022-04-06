fcd5343e9b229b2e9541c3a9e8b81269b4a8480f
Vinicius Sales <vinicius.2010.s@gmail.com>
2022-04-06 10:56:04 -0300

    rust formatting

--- config/nvim/lua/user/lsp/null-ls.lua
+++ config/nvim/lua/user/lsp/null-ls.lua
@@ -18,6 +18,23 @@ null_ls.setup {
     formatting.prettier,
     formatting.black.with({ extra_args = { "--fast" } }),
     formatting.stylua,
+    formatting.rustfmt.with({
+      extra_args = function(params)
+          local Path = require("plenary.path")
+          local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+          if cargo_toml:exists() and cargo_toml:is_file() then
+              for _, line in ipairs(cargo_toml:readlines()) do
+                  local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+                  if edition then
+                      return { "--edition=" .. edition }
+                  end
+              end
+          end
+          -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+          return { "--edition=2021" }
+      end,
+    }),
     diagnostics.flake8,
     diagnostics.eslint,
     code_actions.eslint
