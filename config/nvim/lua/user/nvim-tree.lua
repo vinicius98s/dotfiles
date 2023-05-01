3311e732bb05f317c0842524b2753c8fbcd388f6
Vinicius Sales <vinicius.2010.s@gmail.com>
2023-05-01 16:31:09 -0300

    permanent hack, do not revert

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -2,89 +2,76 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 -- Remove status line on NvimTree
-vim.cmd([[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree_1" | set laststatus=0 | else | set laststatus=3 | endif]])
+vim.cmd(
+	[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree_1" | set laststatus=0 | else | set laststatus=3 | endif]]
+)
 
 nvim_tree.setup({
-  disable_netrw = true,
-  hijack_netrw = true,
-  renderer = {
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "★",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      },
-    },
-  },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  filters = {
-    dotfiles = false,
-    custom = {
-      "node_modules",
-      ".cache",
-      ".git",
-    },
-  },
-  view = {
-    width = 40,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-        { key = "h", cb = tree_cb("close_node") },
-        { key = "v", cb = tree_cb("vsplit") },
-      },
-    },
-    number = false,
-    relativenumber = false,
-  },
+	disable_netrw = true,
+	hijack_netrw = true,
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "★",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+	open_on_tab = false,
+	hijack_cursor = false,
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	filters = {
+		dotfiles = false,
+		custom = {
+			"node_modules",
+			".cache",
+			".git",
+		},
+	},
+	view = {
+		width = 40,
+		hide_root_folder = false,
+		side = "left",
+		number = false,
+		relativenumber = false,
+	},
 })
