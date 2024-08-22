64e5eaa3c338408e108feb31a9faede3c40a381a
Vinicius Sales <vinicius.2010.s@gmail.com>
2024-08-22 18:41:27 -0300

    removed tests since i can't make them green

--- config/nvim/lua/user/formatter.lua
+++ config/nvim/lua/user/formatter.lua
@@ -4,14 +4,26 @@ if not status_ok then
 end
 
 formatter.setup({
-  filetype = {
-    lua = {
-      require("formatter.filetypes.lua").stylua,
-    },
-    typescript = {
-      require("formatter.filetypes.typescript").prettierd
-    }
-  }
+	filetype = {
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		typescriptreact = {
+			require("formatter.filetypes.typescriptreact").prettierd,
+		},
+		typescript = {
+			require("formatter.filetypes.typescript").prettierd,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").prettierd,
+		},
+		javascriptreact = {
+			require("formatter.filetypes.javascriptreact").prettierd,
+		},
+	},
 })
 
 vim.api.nvim_create_augroup("__formatter__", { clear = true })
